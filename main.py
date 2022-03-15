de6cb7fbc515a7c94726268a2b53df8e5051a806
andrk <andrk90@mail.ru>
2022-03-15 15:58:41 +0400

    Test commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def repo_button_click():
     if repo_language_choice == "Python":
         if os.path.exists("task1"):
             os.chdir("task1")
-            exec(open(""))
+            exec(open("123213123"))
 
 
 def open_file():
